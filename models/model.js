9f26d79c7d311e33924e14dd8eb53630e02e464c
userkmitl <59050300@kmitl.ac.th>
2018-10-23 04:20:05 +0700

    fix env

--- models/model.js
+++ models/model.js
@@ -1,7 +1,6 @@
 import {Sequelize as Seq} from "sequelize";
-const envd  = process.env.NODE_ENV || 'development';
-const configd    = require('./../config/config.json')[envd];
-const config    = require('./../config/config.json')[configd.env];
+const env  = process.env.NODE_ENV || 'development';
+const config    = require('./../config/config.json')[env];
 
 const Model = new Seq(config.database, config.username, config.password, {
     host: config.host,
