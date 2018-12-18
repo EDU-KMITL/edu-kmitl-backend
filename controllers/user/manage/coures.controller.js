ccc1dfcfbd82d677ee8de37573a1fe33351121e9
wandee1997 <wandee016@gmail.com>
2018-12-19 02:42:45 +0700

    fix Course

--- controllers/user/manage/coures.controller.js
+++ controllers/user/manage/coures.controller.js
@@ -2,14 +2,14 @@
  * Created by MisterNT on 5/1/2017.
  */
 const UserService = require('./../../../middleware/UserService')
-const { CourseList } = require('./../../../models')
+const { Course } = require('./../../../models')
 
 const UserTearcherCourse = async function (req, res) {
 
     userService = new UserService()
 
     let uid = userService.getUid(req);
-    let CourseLists = await CourseList.findAll({ where: { user_id: uid } }).then((res) => { return res })
+    let CourseLists = await Course.findAll({ where: { user_id: uid } }).then((res) => { return res })
 
     return res.status(200).json({
         success: true,
