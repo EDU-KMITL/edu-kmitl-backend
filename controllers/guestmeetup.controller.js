24c48cb2c71dd38fc8a905b658782f2497b2bcfa
wandee1997 <wandee016@gmail.com>
2018-12-19 03:15:16 +0700

    fix ci

--- controllers/guestmeetup.controller.js
+++ controllers/guestmeetup.controller.js
@@ -6,7 +6,7 @@ const {Meetup} = require('./../models')
 
 const GuestMeetup = async function (req, res) {
 
-   let MeetupRes = await Meetup.findAll({ limit: 20 }).then( (res) => { return res} )
+   let MeetupRes = await Meetup.findAll({ limit: 20 , where: { status:"PUBLIC" }}).then( (res) => { return res} )
       
     res.status(200).json({
         success: true,
