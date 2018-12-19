1105f8230d98dcc49ab337c1de6c47fbe47d2215
wandee1997 <wandee016@gmail.com>
2018-12-19 10:55:07 +0700

    relase Version 1

--- controllers/viewvdieo.controller.js
+++ controllers/viewvdieo.controller.js
@@ -5,9 +5,9 @@ const {VideoList} = require('./../models')
 
 const ViewVideo = async function (req, res) {
     let ViewVideos = await VideoList.findAll({ where: {uuid: req.params.uuid , status:"PUBLIC"} }).then( (res) => { return res} )
+   
     Object.keys(ViewVideos).map(function (key, index) {
         ViewVideos[key].link = ViewVideos[key].link.split("=")[1]
-        return ViewVideos
     });
     res.status(200).json({
         success: true,
